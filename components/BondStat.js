779bdb39f4a34c7165c0f3de5c894821d46ec612
brandendupont-mcw <bdupont@mcw.edu>
2022-07-05 00:33:58 -0400

    Update BondStat.js

--- components/BondStat.js
+++ components/BondStat.js
@@ -2,39 +2,39 @@ const Statistic = () => {
     return (
 
 
-<section class="bg-white mb-8">
-<div class="max-w-screen-xl px-4 mx-auto  sm:px-6 lg:px-8">
+<section className="bg-white mb-8">
+<div className="max-w-screen-xl px-4 mx-auto  sm:px-6 lg:px-8">
 
 
-  <div class="mt-8 ">
-    <dl class="grid grid-cols-1 gap-12 sm:grid-cols-3">
+  <div className="mt-8 ">
+    <dl className="grid grid-cols-1 gap-12 sm:grid-cols-3">
       <div
-        class="flex flex-col px-6  text-center  rounded-lg row-span-2 p-8"
+        className="flex flex-col px-6  text-center  rounded-lg row-span-2 p-8"
       >
-            <p class="text-md font-medium tracking-widest text-gray-800 uppercase lg:text-sm order-last mt-2">
+            <p className="text-md font-medium tracking-widest text-gray-800 uppercase lg:text-sm order-last mt-2">
               Total 2021 Cases 
             </p>
 
-        <h6 class="text-4xl font-bold lg:text-5xl xl:text-5xl">46,261</h6>
+        <h6 className="text-4xl font-bold lg:text-5xl xl:text-5xl">46,261</h6>
       </div>
 
       <div
-        class="flex flex-col px-6  text-center   p-8"
+        className="flex flex-col px-6  text-center   p-8"
       >
-            <p class="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-sm order-last mt-2">
+            <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-sm order-last mt-2">
             Bail Costs Avoided Under the PFA
             </p>
 
-        <h6 class="text-4xl font-bold lg:text-5xl xl:text-5xl ">$48.3M</h6>
+        <h6 className="text-4xl font-bold lg:text-5xl xl:text-5xl ">$48.3M</h6>
       </div>
       <div
-        class="flex flex-col px-6  text-center  p-8"
+        className="flex flex-col px-6  text-center  p-8"
       >
-            <p class="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-sm order-last mt-2">
+            <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-sm order-last mt-2">
             Average Bond Amount Avoided Under the PFA
             </p>
 
-        <h6 class="text-4xl font-bold lg:text-5xl xl:text-5xl">$6,000</h6>
+        <h6 className="text-4xl font-bold lg:text-5xl xl:text-5xl">$6,000</h6>
       </div>
     </dl>
   </div>
